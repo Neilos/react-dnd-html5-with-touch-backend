e698282ce5102fed54f8759b0b46b3c6aa1391a1
Neil Atkinson <neil@bluespot.io>
2016-09-21 09:38:15 +0100

    Wrap HTML5Backend

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-// TODO
+import getEmptyImage from 'react-dnd-html5-backend';
+import NativeTypes from 'react-dnd-html5-backend';
+import HTML5Backend from 'react-dnd-html5-backend';
+
+export { NativeTypes, getEmptyImage };
+
+export default function createHTML5TouchBackend(manager) {
+  return new HTML5Backend(manager);
+}
